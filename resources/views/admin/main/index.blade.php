56abc707d4e11fc3641df85a84f5ee9fa78ee4bf
Dmitry Titov <piratik44@mail.ru>
2022-09-18 19:21:55 +0700

    Update index.blade.php

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -40,25 +40,10 @@
                             <div class="inner">
                                 <h3>#</h3>
 
-                                <p>Посты</p>
+                                <p>Книги</p>
                             </div>
                             <div class="icon">
                                 <i class="far fa-clipboard"></i>
-                            </div>
-                            <a href="#" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                    <!-- ./col -->
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-warning">
-                            <div class="inner">
-                                <h3>#</h3>
-
-                                <p>Категории</p>
-                            </div>
-                            <div class="icon">
-                                <i class="fas fa-th-list"></i>
                             </div>
                             <a href="#" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
@@ -70,7 +55,7 @@
                             <div class="inner">
                                 <h3>#</h3>
 
-                                <p>Тэги</p>
+                                <p>Жанры</p>
                             </div>
                             <div class="icon">
                                 <i class="fas fa-tags"></i>
