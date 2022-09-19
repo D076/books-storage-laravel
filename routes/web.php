632ba67e54dee304db3f875452146743f953cc16
Dmitry Titov <piratik44@mail.ru>
2022-09-19 13:12:25 +0700

    Update web.php
    Create CRUD for genre and user

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController')->name('admin.main.index');
     });
@@ -31,22 +31,22 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::delete('/{book}', 'DeleteController')->name('admin.book.delete');
     });
     Route::group(['namespace' => 'Genre', 'prefix' => 'genres'], function () {
-//        Route::get('/', 'IndexController')->name('admin.genre.index');
-//        Route::get('/create', 'CreateController')->name('admin.genre.create');
-//        Route::post('/', 'StoreController')->name('admin.genre.store');
-//        Route::get('/{genre}', 'ShowController')->name('admin.genre.show');
-//        Route::get('/{genre}/edit', 'EditController')->name('admin.genre.edit');
-//        Route::patch('/{genre}', 'UpdateController')->name('admin.genre.update');
-//        Route::delete('/{genre}', 'DeleteController')->name('admin.genre.delete');
+        Route::get('/', 'IndexController')->name('admin.genre.index');
+        Route::get('/create', 'CreateController')->name('admin.genre.create');
+        Route::post('/', 'StoreController')->name('admin.genre.store');
+        Route::get('/{genre}', 'ShowController')->name('admin.genre.show');
+        Route::get('/{genre}/edit', 'EditController')->name('admin.genre.edit');
+        Route::patch('/{genre}', 'UpdateController')->name('admin.genre.update');
+        Route::delete('/{genre}', 'DeleteController')->name('admin.genre.delete');
     });
     Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
-//        Route::get('/', 'IndexController')->name('admin.user.index');
-//        Route::get('/create', 'CreateController')->name('admin.user.create');
-//        Route::post('/', 'StoreController')->name('admin.user.store');
-//        Route::get('/{user}', 'ShowController')->name('admin.user.show');
-//        Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
-//        Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
-//        Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
+        Route::get('/', 'IndexController')->name('admin.user.index');
+        Route::get('/create', 'CreateController')->name('admin.user.create');
+        Route::post('/', 'StoreController')->name('admin.user.store');
+        Route::get('/{user}', 'ShowController')->name('admin.user.show');
+        Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
+        Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
+        Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
     });
 });
 
