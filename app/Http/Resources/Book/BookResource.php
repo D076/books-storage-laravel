ff9e785b48bc91a4491e1ec96f48c158c9e0e7ce
Dmitry Titov <piratik44@mail.ru>
2022-09-20 15:29:48 +0700

    Create resources

---
+++ app/Http/Resources/Book/BookResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Book;
+
+use App\Http\Resources\Genre\GenreResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'genre_ids' => GenreResource::collection($this->genres),
+            'author' => $this->user->name,
+        ];
+    }
+}
