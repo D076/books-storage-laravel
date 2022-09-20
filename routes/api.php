2ea5c6dc18cecc76581ce4e5ba23cc02b471c6ae
Dmitry Titov <piratik44@mail.ru>
2022-09-20 15:28:07 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Resources\Book\BookResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
+    Route::group(['namespace' => 'Book'], function () {
+        Route::get('/book', 'BookController@books');
+        Route::get('/book/{book}', 'BookController@show');
+        Route::patch('/book/{book}', 'BookController@update');
+        Route::delete('/book/{book}', 'BookController@delete');
+    });
+    Route::group(['namespace' => 'User'], function () {
+        Route::get('/user', 'UserController@users');
+        Route::get('/user/{user}', 'UserController@show');
+        Route::patch('/user/{user}', 'UserController@update');
+    });
 });
