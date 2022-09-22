23724f8b6a1e9b2b348b1612e8ca22d4f7dba0ef
Dmitry Titov <piratik44@mail.ru>
2022-09-22 19:27:03 +0700

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,7 +18,7 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next)
     {
         if ((int)auth()->user()->role !== User::ROLE_ADMIN) {
-            abort(404);
+            return redirect()->route('home');;
         }
         return $next($request);
     }
