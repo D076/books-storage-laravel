78b118e0a13f4496fe505a1beae4adb65a2e6e9a
Dmitry Titov <piratik44@mail.ru>
2022-12-10 16:44:04 +0700

    _ider_helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,114 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property string $title
+ * @property User $user
+ * @property Genre[] $genres
+ * @property int|null $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read int|null $genres_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUserId($value)
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\BookGenre
+ *
+ * @property int $id
+ * @property int $book_id
+ * @property int $genre_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre whereGenreId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookGenre whereUpdatedAt($value)
+ */
+	class BookGenre extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Genre
+ *
+ * @property int $id
+ * @property string $title
+ * @property Book[] $books
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read int|null $books_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereUpdatedAt($value)
+ */
+	class Genre extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int $role
+ * @property Book[] $books
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read int|null $books_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
